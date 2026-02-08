56ead92fc03c4db6581428f792fc31e6b0a6ccbb
Kiorn Pal <kironpal1531@gmail.com>
2026-02-08 11:54:09 +0600

    problem 3 added

---
+++ problem_3.js
@@ -0,0 +1,15 @@
+
+function finalScore(omr) {
+
+    if (typeof omr !== "object" || Array.isArray(omr) || omr.right + omr.wrong + omr.skip !== 100) {
+        return "Invalid";
+    }
+    let rightAnswer = omr.right * +1;
+    let wrongAnswer = omr.wrong * -0.5;
+    let skipQuestion = omr.skip * 0;
+    let score = rightAnswer + wrongAnswer + skipQuestion;
+    return Math.round(score);
+}
+
+
+console.log(finalScore({ right: 67, wrong: 23, skip: 10 }));
